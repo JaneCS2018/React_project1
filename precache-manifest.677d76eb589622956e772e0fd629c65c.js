7ba0a7e47ed0ba9d14533473900c768166dd20e0
Jane <xiangliu2011@gmail.com>
2020-09-22 22:57:07 -0700

    Updates

---
+++ precache-manifest.677d76eb589622956e772e0fd629c65c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40d1f19754ea2c4c4ef652df9f048df",
+    "url": "/JaneCS2018/React_project1/index.html"
+  },
+  {
+    "revision": "8e3294d36632663d09f3",
+    "url": "/JaneCS2018/React_project1/static/css/main.cb77ef2c.chunk.css"
+  },
+  {
+    "revision": "bb0c8d4335c3b2f6e241",
+    "url": "/JaneCS2018/React_project1/static/js/2.560da442.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JaneCS2018/React_project1/static/js/2.560da442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e3294d36632663d09f3",
+    "url": "/JaneCS2018/React_project1/static/js/main.53f28918.chunk.js"
+  },
+  {
+    "revision": "d42aa06ebfb0ed32c679",
+    "url": "/JaneCS2018/React_project1/static/js/runtime-main.f5f6d654.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/JaneCS2018/React_project1/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/JaneCS2018/React_project1/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
